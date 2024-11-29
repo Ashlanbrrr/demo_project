66930fd2332e00fcf810945bb86ff1b07e5196ff
Ashlan <aashlanshahi35@gmail.com>
2024-11-29 08:52:05 +0545

    first commit

---
+++ Properties/model/student.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Properties.model
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string createdby { get; set; }
+        
+    }
+}
